0fc47f159124b983bb6de11de3b7d53afd160cc1
Saat-7 <upadhyaysaatwik@gmail.com>
2025-05-22 09:34:06 +0530

    Add files via upload

---
+++ Assign1_Prob1_sol.py
@@ -0,0 +1,13 @@
+a=input('Enter first number')
+b=input('Enter second number')
+a=int(a)
+b=int(b)
+sum1=a+b
+sub=a-b
+pro=a*b
+div=a/b
+
+print("Addition: ", sum1)
+print("Subtraction: ", sub)
+print("Multiplication: ", pro)
+print("Division: ", div)
